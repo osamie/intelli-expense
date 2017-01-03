f4b6c890bec4530bad0ea37979c1508ec191cf6d
Zuwa Omigie <zuwaomigie@ca.ibm.com>
2017-01-03 00:12:16 -0500

    test mysql connection

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
-from flask import Flask, send_from_directory, render_template
+from flask import Flask, send_from_directory, render_template, request
+from werkzeug.utils import secure_filename
+from flaskext.mysql import MySQL
 
 app = Flask(__name__)
+
+def testDbConnection():
+    #initalize database connection
+    mysql = MySQL()
+    app.config['MYSQL_DATABASE_USER'] = 'root'
+    app.config['MYSQL_DATABASE_DB'] = 'expenses'
+    mysql.init_app(app)
+
+    conn = mysql.connect()
+
+    cursor = conn.cursor()
+
+    query_string = 'SELECT * FROM expenses'
+    cursor.execute(query_string)
+    data = cursor.fetchall()
+    print data[0]
+    conn.close()
+
+
 
 @app.route('/js/<path:path>')
 def send_js(path):
@@ -10,10 +31,12 @@ def send_js(path):
 def index():
     return render_template('home.html')
 
-@app.route('/upload', methods=['POST'])
+@app.route('/uploadFile', methods=['POST'])
 def upload():
-    return 'done'
-
+    f = request.files['the_file']
+    f.save('/var/www/uploads/' + secure_filename(f.filename))
+    print f
+    return render_template('upload.html')
 
 # If ran as a python script
 if __name__ == "__main__":
--- www/app/App.js
+++ www/app/App.js
@@ -1,7 +1,22 @@
 define(['jquery'], function($){
   return {
     start: function(){
-      console.log($(document));
+
+      // var form = $(document).find('form#uploadForm');
+      var inputEl = document.getElementById('userFile');
+
+      // click handler
+      $(document).find('form#uploadForm').submit(function(event){
+        event.preventDefault();
+
+        $.ajax({
+          type: 'POST',
+          url: '/uploadFile',
+          contentType: 'text/csv',
+          data: inputEl.files[0]
+        });
+
+      });
     }
   };
 });
